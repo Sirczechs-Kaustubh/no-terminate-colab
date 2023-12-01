f53d46017fcecc040c3cdb9b345b74ac6c034b64
Sirczechs-Kaustubh <128580953+Sirczechs-Kaustubh@users.noreply.github.com>
2023-12-01 23:25:31 +0000

    Update anti-termination.js

--- anti-termination.js
+++ anti-termination.js
@@ -1,10 +1,10 @@
 function ConnectButton(){
-    console.log("Connect pushed"); 
+    console.log("Connect button pressed"); 
     document.querySelector("#top-toolbar > colab-connect-button").shadowRoot.querySelector("#connect").click() 
 }
 
-var colab = setInterval(ConnectButton,60000);
+var press = setInterval(ConnectButton,60000);
 
 // uncomment the below line and use it for clearing
 
-//clearInterval(colab)
+//clearInterval(press)
